7db609fabb8b55d54cdfb9947f60faa05f3d499c
Kamil Saeed <kamilsaeedkamali18@gmail.com>
2025-10-17 18:02:27 +0500

    Added first Python program

---
+++ basic/firstprogram.py
@@ -0,0 +1,55 @@
+print("My name is Kamil Saeed", "I am 20 years old")
+
+print(23)
+print(23+34)
+
+name = "Kamil"
+age = 20
+price = 25.99
+old = False
+a = None
+
+print("\nMy name is", name)
+print("My age is:", age)
+
+print("")
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
+# single comment
+
+"""
+Multi-line
+comments
+"""
+
+print("")
+
+# Type Conversion
+
+a = "2"
+b = 4.25
+
+c = int(a)
+sum = c + b
+
+print(sum)
+
+name2 = input("\nEnter your name: ")
+print("Welcome,", name2)
+
+var = int(input("\nEnter some value: "))
+print("Value entered is:", var)
+
+print("\nAverage of 2 floating numbers")
+
+var1 = float(input("\nEnter num1: "))
+var2 = float(input("Enter num2: "))
+
+sum = var1 + var2
+
+print("\nAverage is:", sum / 2)
